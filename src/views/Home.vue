d3791154594a4ed35ad036474ccd2cf9fbdddb3e
MarkNewcomb1 <wlmmn@hotmail.com>
2018-08-28 11:17:59 -0600

    Home view functional

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <Header />
+    <router-view></router-view>
+    <h1>This be home</h1>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header';
+
+export default {
+    name: "Home",
+    components: {
+    Header
+    }
+};
+</script>
+
+<style scoped>
+</style>
